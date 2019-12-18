c957a4b33de7b52a0d41b6204fef11931283bb4f
Elnur <mes.elnur@gmail.com>
2019-12-18 21:08:29 +0200

    query

--- resources/views/pages/data.blade.php
+++ resources/views/pages/data.blade.php
@@ -1,8 +1,24 @@
 @extends('layouts.default')
 @section('content')
     <div class="container">
-        <img src="{{ asset('image/schema.png') }}" alt="schema">
-        <button><a href="downloads/schema.sql">Download SQL File</a></button>
+        <div class="row">
+
+            <div class="col-md-12">
+                <img src="{{ asset('image/schema.png') }}" alt="schema">
+            </div>
+
+            <div class="col-md-6">
+                <code>SELECT pt.EMPLOYEE_ID, o.NAME, pt.EXPERIENCE, pt.INTRODUCTION,pt.EDUCATION FROM employees o, translation pt, language l WHERE o.ID = pt.EMPLOYEE_ID AND pt.LANGUAGE_CODE = l.CODE AND o.id = 1</code>
+            </div>
+
+            <div class="col-md-6">
+                <img src="{{ asset('image/query.png') }}" alt="schema">
+            </div>
+
+            <div class="col-md-6">
+                <button><a href="downloads/schema.sql">Download SQL File</a></button>
+            </div>
+        </div>
     </div>
 
 @stop
